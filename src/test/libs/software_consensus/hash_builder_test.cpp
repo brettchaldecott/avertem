43313666b43f3ea1458227776fe78da491c39d85
Brett Chaldecott <brett.chaldecott@gmail.com>
2018-06-13 03:08:51 +0000

    1) Fixed rename code

--- src/test/libs/software_consensus/hash_builder_test.cpp
+++ src/test/libs/software_consensus/hash_builder_test.cpp
@@ -28,8 +28,8 @@
 BOOST_AUTO_TEST_CASE( hash_builder_test ) {
     
     std::cout << "The obfuscated string : " << DEF_OBFUSCATED("cd6f953fdc6d6011f27667fc3267cb9f0e6fa962 $").decrypt() << std::endl;
-    std::cout << "The version : " << keto::software_consensus::Constants::getVERSION() << std::endl;
-    std::cout << "The source version : " << keto::software_consensus::Constants::getSourceVERSION() << std::endl;
+    std::cout << "The version : " << keto::software_consensus::Constants::getVersion() << std::endl;
+    std::cout << "The source version : " << keto::software_consensus::Constants::getSourceVersion() << std::endl;
     
     
 }
