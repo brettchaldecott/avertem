bf289b197ca17b3988e4e0bd460920130ecfc0af
Brett Chaldecott <brett.chaldecott@gmail.com>
2018-09-28 09:22:48 +0000

    1) Fixed bash upgrade script

--- scripts/upgrade/ubuntu.sh
+++ scripts/upgrade/ubuntu.sh
@@ -12,7 +12,7 @@ fi
 echo "Download the version $SERVER_VERSION"
 wget https://s3-eu-west-1.amazonaws.com/keto-release/linux/ubuntu/$SERVER_VERSION/keto_shared_$SERVER_VERSION.tar.gz -O $KETO_HOME/tmp/keto_shared_$SERVER_VERSION.tar.gz
 
-cd $KETO_HOME/shared/ && tar -zxvf ../$KETO_HOME/tmp/keto_shared_$SERVER_VERSION.tar.gz
+cd $KETO_HOME/shared/ && tar -zxvf $KETO_HOME/tmp/keto_shared_$SERVER_VERSION.tar.gz
 cd $CURRENT_DIR
 echo "$SERVER_VERSION" > ${KETO_HOME}/config/keto_version
 echo "Upgrade complete"
