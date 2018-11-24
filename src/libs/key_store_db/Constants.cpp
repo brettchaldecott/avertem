f130737b8e2d3cbc6c8cdfc41bd9ae27784af916
Brett Chaldecott <brett.chaldecott@absa.co.za>
2018-11-24 08:04:02 +0200

    1) Added the beginnings of the key store db

---
+++ src/libs/key_store_db/Constants.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Constants.cpp
+ * Author: brettchaldecott
+ * 
+ * Created on 24 November 2018, 7:18 AM
+ */
+
+#include "Constants.hpp"
+
+Constants::Constants() {
+}
+
+Constants::Constants(const Constants& orig) {
+}
+
+Constants::~Constants() {
+}
+
---
+++ src/libs/key_store_db/include/keto/key_store_db/Constants.hpp
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Constants.hpp
+ * Author: brettchaldecott
+ *
+ * Created on 24 November 2018, 7:18 AM
+ */
+
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#include <vector>
+#include <string>
+
+#include "keto/obfuscate/MetaString.hpp"
+
+
+class Constants {
+public:
+    Constants() = delete;
+    Constants(const Constants& orig) = delete;
+    virtual ~Constants() = delete;
+    
+    
+    
+private:
+
+};
+
+#endif /* CONSTANTS_HPP */
+
