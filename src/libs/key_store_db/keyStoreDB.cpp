d3c39ed56945eb4c77d413cc95fadb1fb8ab608c
Brett Chaldecott <brett.chaldecott@absa.co.za>
2018-11-28 19:53:15 +0200

    update

--- src/libs/key_store_db/keyStoreDB.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Brett Chaldecott on 2018/11/24.
-//
-
-#include "keyStoreDB.hpp"
--- src/libs/key_store_db/keyStoreResourceManager.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Brett Chaldecott on 2018/11/25.
-//
-
-#include "keyStoreResourceManager.hpp"
