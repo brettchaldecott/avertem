4c0f56dc0005ec7aaa3ad1b9a1286257aa2d3f13
Brett Chaldecott <brett.chaldecott@absa.co.za>
2020-05-12 21:08:22 +0200

    1) updated key generation to copy overlap to beginning

--- scripts/tools/GenerateKeys.sh
+++ scripts/tools/GenerateKeys.sh
@@ -2,6 +2,11 @@
 
 number=$1
 outputDir=$2
+overlap=$3
+
+if [[ -z "${overlap}" ]]; then
+    overlap=0
+fi
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -9,7 +14,18 @@ if [ ! -f "${SOURCE_DIR}/../../deps_build/build/install/bin/avertem_tools.sh" ] 
         exit 0
 fi
 
-for (( count=1; count<=${number}; count++ ))
+zero=0
+if [[ "${overlap}" -ne "${zero}" ]]; then
+    startPoint=${number-overlap}
+    for (( count=1; count<= ${overlap}; count++ ))
+    do
+        echo "mv -f ${outputDir}/key_${startPoint+count}.json ${outputDir}/key_${count}.json"
+        mv -f "${outputDir}/key_${startPoint+count}.json" "${outputDir}/key_${count}.json"
+    done
+fi
+
+
+for (( count=${overlap+1}; count<=${number}; count++ ))
 do
    echo "${SOURCE_DIR}/../../deps_build/build/install/bin/avertem_tools.sh -G > \"${outputDir}/key_${count}.json\""
    ${SOURCE_DIR}/../../deps_build/build/install/bin/avertem_tools.sh -G > "${outputDir}/key_${count}.json"
--- scripts/tools/GenerateKeysModuleKeys.sh
+++ scripts/tools/GenerateKeysModuleKeys.sh
@@ -2,17 +2,37 @@
 
 sourceDir=$1
 outputDir=$2
+overlap=$3
 
-sourceKeys=(`find $sourceDir -name "*.json"`)
+if [[ -z "${overlap}" ]]; then
+    overlap=0
+fi
+
+sourceKeys=$(find $sourceDir -name "*.json")
+number=$(find $sourceDir -name "*.json" | wc -l)
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ ! -f "${SOURCE_DIR}/../../deps_build/build/install/bin/avertem_tools.sh" ] ; then
         exit 0
 fi
 
+zero=0
+if [[ "${overlap}" -ne "${zero}" ]]; then
+    startPoint=${number-overlap}
+    for (( count=1; count<= ${overlap}; count++ ))
+    do
+        echo "mv -f ${outputDir}/key_${startPoint+count}.json ${outputDir}/key_${count}.json"
+        mv -f "${outputDir}/key_${startPoint+count}.json" "${outputDir}/key_${count}.json"
+    done
+fi
+
+count=1;
 for sourceKey in "${sourceKeys[@]}";
 do
-    filename=${sourceKey##*/}
-    echo "${SOURCE_DIR}/../../deps_build/build/install/bin/avertem_tools.sh -G -k ${sourceKey} > \"${outputDir}/${filename}\""
-    ${SOURCE_DIR}/../../deps_build/build/install/bin/avertem_tools.sh -G -k ${sourceKey} > "${outputDir}/${filename}"
+    if [[ "${count}" -ge "${overlap}" ]]; then
+        filename=${sourceKey##*/}
+        echo "${SOURCE_DIR}/../../deps_build/build/install/bin/avertem_tools.sh -G -k ${sourceKey} > \"${outputDir}/${filename}\""
+        ${SOURCE_DIR}/../../deps_build/build/install/bin/avertem_tools.sh -G -k ${sourceKey} > "${outputDir}/${filename}"
+    fi
+    count++
 done
