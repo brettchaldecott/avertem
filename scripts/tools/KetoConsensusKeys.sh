f5d702dc57c0c507d805770c622f1d43ebae8231
Brett Chaldecott <brett.chaldecott@absa.co.za>
2020-06-02 13:27:38 +0200

    1) fixed the scripts

--- scripts/tools/KetoConsensusKeys.sh
+++ scripts/tools/KetoConsensusKeys.sh
@@ -16,6 +16,7 @@ for (( count=1; count<=${number}; count++ ))
 do
     echo "${SOURCE_DIR}/../../deps_build/build/install/bin/avertem_tools.sh -P -k \"${directoryWithKeys}/key_${count}.json\""
     privateBytes=`${SOURCE_DIR}/../../deps_build/build/install/bin/avertem_tools.sh -P -k "${directoryWithKeys}/key_${count}.json"`
+    echo "Bytes from file ${directoryWithKeys}/key_${count}.json : ${privateBytes}"
     privateKeyBytes="${privateKeyBytes}${seperator}${privateBytes}"
     seperator=","
 done
