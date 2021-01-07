1a6d9447e6fd3192586e1254b7050bd9a377622e
Brett Chaldecott <brett.chaldecott@absa.co.za>
2021-01-07 17:48:24 +0200

    1) upgrade script

--- scripts/upgrade/ubuntu.sh
+++ scripts/upgrade/ubuntu.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
+
 VERSION=`cat ${KETO_HOME}/config/avertem_version`
 SERVER_VERSION=`wget -qO- https://s3-eu-west-1.amazonaws.com/avertem/linux/ubuntu/18.04/latest_version.txt`
 CURRENT_DIR=`pwd`
+VERSION_RESULLT=$(vercomp $VERSION $SERVER_VERSION)
 
-if [ "$VERSION" == "$SERVER_VERSION" ] ; then
+if [ "$VERSION_RESULT" -eq "0" ] || [ "$VERSION_RESULT" -eq "1" ] ; then
     echo "Versions are the same nothing to be done"
     exit 0
 fi
